b70e68fd58575612edfbde1be3eb2353b449c806
Dipling <oscar.macdonald@outlook.com>
2026-01-07 14:04:50 +0000

    keycard item finished
    (idk if doors work yet)

--- core/src/main/java/io/github/team6ENG/EscapeUni/Keycard.java
+++ core/src/main/java/io/github/team6ENG/EscapeUni/Keycard.java
@@ -1,9 +1,12 @@
 package io.github.team6ENG.EscapeUni;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import space.earlygrey.shapedrawer.ShapeDrawer;
 
 public class Keycard extends RoomObject {
     private int gridX, gridY;
+    public boolean collected = false;
 
     public Keycard(float x, float y) {
         super(NewGameScreen.keycardTex, x, y);
@@ -13,8 +16,19 @@ public class Keycard extends RoomObject {
 
     @Override
     public void step(float delta) {
-        if (NewGameScreen.player.gridInstance.getGridX() == gridX && NewGameScreen.player.gridInstance.getGridY() == gridY) {
-            System.out.println("Keycard collision");
+        if (!collected) {
+            if (NewGameScreen.player.gridInstance.getGridX() == gridX && NewGameScreen.player.gridInstance.getGridY() == gridY) {
+                NewGameScreen.infoMsg("Keycard collected.");
+                collected = true;
+                NewGameScreen.keycard = true;
+            }
+        }
+    }
+
+    @Override
+    public void draw(ShapeDrawer drawer, SpriteBatch batch) {
+        if (!collected) {
+            batch.draw(texture, x, y);
         }
     }
 }
